f380063fedd8029d005f2818394afda83648411b
João Tury <jgtb313@gmail.com>
2025-03-13 13:40:07 -0300

    Adicionando DatabaseModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 
+import { DatabaseModule } from '@/adapters/database';
+
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    DatabaseModule.register(),
   ],
   controllers: [],
   providers: [],
